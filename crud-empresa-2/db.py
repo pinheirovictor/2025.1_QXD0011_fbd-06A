6183b2ddfc65c60a927cfcbe2c2f8a08e3b2c934
Francisco Victor da Silva Pinheiro <37351953+pinheirovictor@users.noreply.github.com>
2025-07-24 13:41:57 -0300

    Delete crud-empresa-2 directory

--- crud-empresa-2/db.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-def get_connection():
-    return psycopg2.connect(
-        dbname="empresa",
-        user="postgres",
-        password="2023",
-        host="localhost"
-    )
